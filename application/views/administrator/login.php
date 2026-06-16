5586ebab38a0948832e5b14e52f481138b2f5a16
matien19 <matienkasa@gmail.com>
2026-06-17 03:06:17 +0700

    base-url login

--- application/views/administrator/login.php
+++ application/views/administrator/login.php
@@ -7,10 +7,11 @@
     <title>Login Administrator</title>
 
     <!-- SB Admin 2 CSS -->
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="<?= base_url('assets/css/sb-admin-2.min.css') ?>" rel="stylesheet">
 
     <!-- Font -->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+    <link href="<?= base_url('assets/vendor/fontawesome-free/css/all.min.css') ?>" rel="stylesheet">
+
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,300,400,600,700,800,900" rel="stylesheet">
 
     <!-- CUSTOM STYLE -->
@@ -57,26 +58,26 @@
                     </div>
 
                     <!-- FORM LOGIN -->
-                    <form method="post" action="<?php echo base_url('index.php/administrator/auth/proses_login')?>">
+                    <form method="post" action="<?php echo base_url('index.php/administrator/auth/proses_login') ?>">
 
-    <div class="form-group mb-3">
-        <input type="text" name="username"
-            class="form-control form-control-user"
-            placeholder="Username Anda">
-            <?= form_error('username', '<small class="text-danger">', '</small>'); ?>
+                        <div class="form-group mb-3">
+                            <input type="text" name="username"
+                                class="form-control form-control-user"
+                                placeholder="Username Anda">
+                            <?= form_error('username', '<small class="text-danger">', '</small>'); ?>
 
-    </div>
+                        </div>
 
-    <div class="form-group mb-4">
-        <input type="password" name="password"
-            class="form-control form-control-user"
-            placeholder="Password Anda">
-            <?= form_error('password', '<small class="text-danger">', '</small>'); ?>
-    </div>
+                        <div class="form-group mb-4">
+                            <input type="password" name="password"
+                                class="form-control form-control-user"
+                                placeholder="Password Anda">
+                            <?= form_error('password', '<small class="text-danger">', '</small>'); ?>
+                        </div>
 
-    <button class="btn btn-primary btn-user btn-block">Login</button>
+                        <button class="btn btn-primary btn-user btn-block">Login</button>
 
-</form>
+                    </form>
                     <!-- END FORM -->
 
                 </div>
@@ -87,9 +88,9 @@
     </div>
 
     <!-- JS -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="js/sb-admin-2.min.js"></script>
+    <script src="<?= base_url('assets/vendor/jquery/jquery.min.js') ?>"></script>
+    <script src="<?= base_url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/sb-admin-2.min.js') ?>"></script>
 
 </body>
 
