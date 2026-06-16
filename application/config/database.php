03cec3202e2bce5169e2af48e71ea0da70c19e50
matien19 <matienkasa@gmail.com>
2026-06-17 01:32:23 +0700

    db

--- application/config/database.php
+++ application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
     'dsn'   => '',
-    'hostname' => 'localhost',
+    'hostname' => '117.53.46.60',
     'username' => 'root',
-    'password' => '',
+    'password' => 'bunga2026',
     'database' => 'rekapitulasi_saksi',
     'dbdriver' => 'mysqli',
     'dbprefix' => '',
